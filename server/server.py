cf04b35629177d6561cf8b28a3620d1a58a09eaf
dalva24 <dariel24@gmail.com>
2015-04-26 08:50:26 +0700

    added all basic interactions. TODO debug

--- server/server.py
+++ server/server.py
@@ -55,7 +55,7 @@ class Inventory:
 
 	def collect(self, name, amount):
 		self.item[name] += amount
-		return True
+		return self.item2id[name]
 
 	def mix(self, name1, name2):
 		name1 = self.id2item[name1]
@@ -68,11 +68,10 @@ class Inventory:
 					resultantTier = int(name1[1]) + 1
 					if int(name1[2] < name2[2]):
 						self.item['R'+str(resultantTier)+name1[2]] += 1
-						self.newestItem = self.item2id['R'+str(resultantTier)+name1[2]]
+						return self.item2id['R'+str(resultantTier)+name1[2]]
 					else:
 						self.item['R'+str(resultantTier)+name2[2]] += 1
-						self.newestItem = self.item2id['R'+str(resultantTier)+name2[2]]
-					return True
+						return self.item2id['R'+str(resultantTier)+name2[2]]
 				else:
 					raise Fail("Unable to craft: insufficient ingredients")
 			else:
@@ -80,11 +79,9 @@ class Inventory:
 		else:
 			raise Fail("Unable to craft: cannot mix different tiers")
 
-
-
-def xchange(self, bought, sold, boughtAmount, soldAmount):
-		self.item[bought] += boughtAmount
-		self.item[sold] -= soldAmount
+	def xchange(self, bought, sold, boughtAmount, soldAmount):
+			self.item[bought] += boughtAmount
+			self.item[sold] -= soldAmount
 
 
 class User:
@@ -112,13 +109,18 @@ class User:
 		]
 
 	def move(self, newLoc):
-		if self.newLocation is None:
-			self.newLocation = newLoc
-			self.moveTime = random.randint(60,120)
+		if int(self.newLocation[0]) is int(self.location[0])+1 or int(self.newLocation[0]) is int(self.location[0])-1:
+			if int(self.newLocation[1]) is int(self.location[1])+1 or int(self.newLocation[1]) is int(self.location[1])-1:
+				self.newLocation = newLoc
+				self.moveTime = random.randint(60,120)   # TODO: time, and what happens when time reaches zero? threads?
+				return self.moveTime
+			else:
+				raise Fail("move location is too far")
+		else:
+			raise Fail("move location is too far")
 
 
 class UserContainer:
-	CUR_USR = None
 	user = []
 
 	def __init__(self):
@@ -132,8 +134,7 @@ class UserContainer:
 					time = str(datetime.datetime.now())
 					usr.token = hashlib.md5(uname.encode()+pw.encode()+time.encode()).hexdigest()
 					usr.location = str(random.randint(0, MAP.width-1)) + str(random.randint(0, MAP.height-1))
-					self.CUR_USR = usr
-					return True
+					return usr
 				else:
 					raise Fail("username/password combination is not found")
 		raise Fail("username/password combination is not found")
@@ -156,13 +157,30 @@ class UserContainer:
 			# i have NO IDEA how that ^ works, but whatever -_- just works.
 			# indent optional.
 
-	def get(self, token):
+	def mix(self, token, name1, name2):
 		for usr in self.user:
 			if usr.token is token:
-				self.CUR_USR = usr
-				return True
+				return usr.inv.mix(name1, name2)
 		raise Fail("User not found")
 
+	def inv(self, token):
+		for usr in self.user:
+			if usr.token is token:
+				return usr.getInventory()
+		raise Fail("User not found")
+
+	def move(self, token, newLoc):
+		for usr in self.user:
+			if usr.token is token:
+				return usr.move(newLoc)
+		raise Fail("User not found")
+
+	def collect(self, token):
+		for usr in self.user:
+			if usr.token is token:
+				item = MAP.items[usr.location[0]][usr.location[1]]
+				return usr.inv.collect(item, 1)
+		raise Fail("User not found")
 
 class Map:
 	def __init__(self):
@@ -200,17 +218,17 @@ UC = UserContainer()
 
 BUFFER_SIZE = 4096
 
-print("Broadcasting self existence to Tracker...")
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((TRAC_IP, TRAC_PORT))
-a = json.dumps({"method": "join", "ip": TCP_IP, "port": TCP_PORT})
-s.send(a.encode('utf-8'))
-packet = json.loads(s.recv(BUFFER_SIZE))
-if packet['status'] == 'ok':
-	OTHER_SERVERS = packet['value']
-else:
-	sys.exit("ERROR: " + packet['description'])
-s.close()
+#print("Broadcasting self existence to Tracker...")
+#s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#s.connect((TRAC_IP, TRAC_PORT))
+#a = json.dumps({"method": "join", "ip": TCP_IP, "port": TCP_PORT})
+#s.send(a.encode('utf-8'))
+#packet = json.loads(s.recv(BUFFER_SIZE))
+#if packet['status'] == 'ok':
+#	OTHER_SERVERS = packet['value']
+#else:
+#	sys.exit("ERROR: " + packet['description'])
+#s.close()
 
 print("Binding port...")
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -227,108 +245,116 @@ SRVT = "0"  # TODO TIME
 
 # Main Loop =============================================================================
 
-
 print("Server Init Completed. Listening...")
 while MAIN_LOOP is True:
 	conn, addr = s.accept()
 	data = conn.recv(BUFFER_SIZE)
 	print("New LoliCON")
-	if data != "":
+	if data is "":
 		print("PING")
 	else:  # handleJSON
-		packet = json.loads(data)
-		if packet['method'] == 'serverStatus':  # TAIGA #
-			print("nop")
-		elif packet['method'] == 'signup':
-			try:
-				if UC.signup(packet['username'], packet['password']):
-					msg = {'status': 'ok'}
-					conn.send(json.dumps(msg).encode('utf-8'))
-				else:
-					msg = {'status': 'error'}
-					conn.send(json.dumps(msg).encode('utf-8'))
-			except Fail as e:
-				msg = {
-					'status': 'fail',
-					'description': e.msg
-				}
-				conn.send(json.dumps(msg).encode('utf-8'))
-		elif packet['method'] == 'login':
-			try:
-				if UC.login(packet['username'], packet['password']):
-					msg = {
-						'status': 'ok',
-						'token': UserContainer.CUR_USR.token,
-						'x': UserContainer.CUR_USR.location[0],
-						'y': UserContainer.CUR_USR.location[1],
-						'time': SRVT  # TODO TIME
-					}
-					conn.send(json.dumps(msg).encode('utf-8'))
-				else:
-					msg = {'status': 'error'}
-					conn.send(json.dumps(msg).encode('utf-8'))
-			except Fail as e:
-				conn.send('{"status":"fail","description":"'+e.msg+'"}')
-		elif packet['method'] == 'inventory':
-			try:
-				if UC.get(packet['token']):
-					msg = {
-						'status': 'ok',
-						'inventory': str(UserContainer.CUR_USR.getInventory)
-					}
-					conn.send(json.dumps(msg).encode('utf-8'))
-				else:
-					msg = {'status': 'error'}
-					conn.send(json.dumps(msg).encode('utf-8'))
-			except Fail as e:
-				msg = {'status': 'error'}
-				conn.send(json.dumps(msg).encode('utf-8'))
-		elif packet['method'] == 'mixitem':
-			try:
-				if UC.get(packet['token']):
-					if UserContainer.CUR_USR.mix(packet['item1'], packet['item2']):
-						msg = {
-							'status': 'ok',
-							'item': UserContainer.CUR_USR.newestItem
-						}
+		try:
+			packet = json.loads(data)
+			if packet['method'] == 'serverStatus':  # TAIGA #
+				print("nop")
+			elif packet['method'] == 'signup':
+				try:
+					if UC.signup(packet['username'], packet['password']):
+						msg = {'status': 'ok'}
 						conn.send(json.dumps(msg).encode('utf-8'))
 					else:
 						msg = {'status': 'error'}
 						conn.send(json.dumps(msg).encode('utf-8'))
-				else:
+				except Fail as e:
+					msg = {
+						'status': 'fail',
+						'description': e.msg
+					}
+					conn.send(json.dumps(msg).encode('utf-8'))
+			elif packet['method'] == 'login':
+				try:
+					usrv = UC.login(packet['username'], packet['password'])
+					msg = {
+						'status': 'ok',
+						'token': usrv.token,
+						'x': usrv.location[0],
+						'y': usrv.location[1],
+						'time': SRVT  # TODO TIME
+					}
+					conn.send(json.dumps(msg).encode('utf-8'))
+				except Fail as e:
+					conn.send('{"status":"fail","description":"'+e.msg+'"}')
+			elif packet['method'] == 'inventory':
+				try:
+					msg = {
+						'status': 'ok',
+						'inventory': str(UC.inv(packet['token']))
+					}
+					conn.send(json.dumps(msg).encode('utf-8'))
+				except Fail as e:
 					msg = {'status': 'error'}
 					conn.send(json.dumps(msg).encode('utf-8'))
-			except Fail as e:
-				conn.send('{"status":"fail","description":"'+e.msg+'"}')
-		elif packet['method'] == 'map':
-			print("nop")
-		elif packet['method'] == 'move':
-			print("nop")
-		elif packet['method'] == 'field':
-			print("nop")
-		elif packet['method'] == 'offer':
-			print("nop")
-		elif packet['method'] == 'tradebox':
-			print("nop")
-		elif packet['method'] == 'sendfind':
-			print("nop")
-		elif packet['method'] == 'findoffer':
-			print("nop")
-		elif packet['method'] == 'sendaccept':
-			print("nop")
-		elif packet['method'] == 'accept':
-			print("nop")
-		elif packet['method'] == 'fetchitem':
-			print("nop")
-		elif packet['method'] == 'killserver':
-			if packet['magicString'] == 'q34tAq34tb3qy4IUaXa4t':
-				MAIN_LOOP = False
+			elif packet['method'] == 'mixitem':
+				try:
+					msg = {
+						'status': 'ok',
+						'item': UC.mix(packet['token'], packet['item1'], packet['item2'])
+					}
+					conn.send(json.dumps(msg).encode('utf-8'))
+				except Fail as e:
+					conn.send('{"status":"fail","description":"'+e.msg+'"}')
+			elif packet['method'] == 'map':
+				msg = {
+					'status': 'ok',
+					'name': MAP.name,
+					'width': MAP.width,
+					'height': MAP.height
+				}
+				conn.send(json.dumps(msg).encode('utf-8'))
+			elif packet['method'] == 'move':
+				try:
+					msg = {
+						'status': 'ok',
+						'time': UC.move(packet['token'], str(packet['x'] + str(packet['y'])))  # TODO TIME
+					}
+					conn.send(json.dumps(msg).encode('utf-8'))
+				except Fail as e:
+					conn.send('{"status":"fail","description":"'+e.msg+'"}')
+			elif packet['method'] == 'field':
+				try:
+					msg = {
+						'status': 'ok',
+						'item': UC.collect(packet['token'])
+					}
+					conn.send(json.dumps(msg).encode('utf-8'))
+				except Fail as e:
+					conn.send('{"status":"fail","description":"'+e.msg+'"}')
+			elif packet['method'] == 'offer':
+				print("nop")
+			elif packet['method'] == 'tradebox':
+				print("nop")
+			elif packet['method'] == 'sendfind':
+				print("nop")
+			elif packet['method'] == 'findoffer':
+				print("nop")
+			elif packet['method'] == 'sendaccept':
+				print("nop")
+			elif packet['method'] == 'accept':
+				print("nop")
+			elif packet['method'] == 'fetchitem':
+				print("nop")
+			elif packet['method'] == 'killserver':
+				if packet['magicString'] == 'q34tAq34tb3qy4IUaXa4t':
+					MAIN_LOOP = False
+				else:
+					print("WARNING: intrusion attempt")
 			else:
-				print("WARNING: intrusion attempt")
-		else:
-			print("unknown connection method.")
+				print("unknown connection method.")
+		except Exception as ex:
+			print("ERROR: unknown exception in handling connection data.")
+			print(ex)
 
-	conn.shutdown('SHUT_RDWR')
+	#conn.shutdown('SHUT_RDWR')
 	conn.close()
 	UC.save()  # save aaaall the time. really safe for times when server suddenly crash for no reason whatsoever.
 
